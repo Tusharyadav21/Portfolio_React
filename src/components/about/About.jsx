0a2f00e115ff312d58c3766e8d283b0c57ac495a
Tusharyadav21 <tusharydv@hotmail.com>
2021-12-12 18:46:42 +0530

    Added images

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,9 @@ const About = () => {
             </div>
             <div className="a-right">
                 <h1 className="a-title">About Me</h1>
-                <p className="a-sub">Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae</p>
+                <p className="a-sub">To learn, work and grow for the betterment of the organization and myself. Seeking for the
+                            opportunity to contribute my knowledge, skills, experience and giving the best for the
+                            advancement of the organization while making my skills stronger.</p>
                 <p className="a-desc">Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
             </div>
         </div>
--- src/data.js
+++ src/data.js
@@ -2,32 +2,32 @@ export const products = [
 
     {
         id: 1,
-        img: "https.//api.unsplash.com/search/photos?",
-        link: "https://www.instagram.com/pctures_que",
+        img: "https://picsum.photos/600",
+        link: "https://picsum.photos/",
     },
     {
         id: 2,
-        img: "https://unsplash.com/photos/uUFVmiNdt14",
-        link: "https://www.instagram.com/pctures_que",
+        img: "https://picsum.photos/1100",
+        link: "https://picsum.photos/",
     },
     {
         id: 3,
-        img: "https://api.unsplash.com/search/photos?count=1",
-        link: "https://www.instagram.com/pctures_que",
+        img: "https://picsum.photos/400",
+        link: "https://picsum.photos/",
     },
     {
         id: 4,
-        img: "https.//api.unsplash.com/search/photos?",
-        link: "https://www.instagram.com/pctures_que",
+        img: "https://picsum.photos/500",
+        link: "https://picsum.photos/",
     },
     {
         id: 5,
-        img: "https.//api.unsplash.com/search/photos?",
-        link: "https://www.instagram.com/pctures_que",
+        img: "https://picsum.photos/1000",
+        link: "https://picsum.photos/",
     },
     {
         id: 6,
-        img: "https.//api.unsplash.com/search/photos?",
-        link: "https://www.instagram.com/pctures_que",
+        img: "https://picsum.photos/900",
+        link: "https://picsum.photos",
     },
 ];
