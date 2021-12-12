8b9832b948154df5bb3b22f961d9711940101993
Tusharyadav21 <tusharydv@hotmail.com>
2021-12-12 19:46:43 +0530

    Text change

--- src/components/productlist/ProductList.jsx
+++ src/components/productlist/ProductList.jsx
@@ -6,8 +6,9 @@ const ProductList = () => {
     return (
         <div className="pl">
             <div className="pl-texts">
-                <h1 className="pl-title">Create & Inspire.</h1>
-                <p className="pl-desc">I'm Creating a portfolio that your work has been waiting ..... Beatiful homes, Stunning portfolio styles & a whole lot more....... inside</p>
+                <h1 className="pl-title">Learning & Creating.</h1>
+                <p className="pl-desc">This my <b>portfolio</b>and I'm learning new skills day by day and implementing it on regular basis.
+                 I'm creative, hardworking and techy person. By the way these are not my pictures.</p>
             </div>
             <div className="pl-list">
                 {products.map((item) => (
